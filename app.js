0db4f90358982a9bc097825bd8bc64eebfbff155
Ethan Dodgen <thefivalco@gmail.com>
2022-11-01 07:41:37 -0500

    added seperate routes, html, and css

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 // core module imports
 // third party package
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const { response } = require('express');
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+// const { response } = require('express')
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(express.static(path.join(__dirname, 'public'))) //serves files statically, comes with express
 
 //outsourced routes
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes)
+app.use('/shop', shopRoutes)
 
 //cath all route
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found</h1>")
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
-app.listen(3000);
+app.listen(3000)
---
+++ util/path.js
@@ -0,0 +1,4 @@
+const path = require('path')
+// import { dirname } from 'path'
+
+module.exports = path.dirname(require.main.filename)

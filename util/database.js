8be71cb9bafdb1da07f775f25e7355b67f5bde32
Ethan Dodgen <thefivalco@gmail.com>
2022-12-18 14:03:11 -0600

    test push

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('node-complete', 'root', 'Bangarang00!!', { 
-    dialect: 'mysql', 
-    host: 'localhost' 
+const sequelize = new Sequelize('node-complete', 'root', 'bangarang', {
+    dialect: 'mysql',
+    host: 'localhost'
 })
 
 module.exports = sequelize

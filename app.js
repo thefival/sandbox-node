f915772f1c3428c9414e26563a67748e9687743a
Ethan Dodgen <thefivalco@gmail.com>
2022-12-19 14:11:07 -0600

    added user model

--- app.js
+++ app.js
@@ -5,29 +5,58 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const errorController = require('./controllers/error')
+const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
+
 const app = express()
 
 //sets template engine
 app.set('view engine', 'ejs')
-// app.set('views', 'views')
+app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-const errorController = require('./controllers/error')
-// const db = require('./util/database')
-const sequelize = require('./util/database')
+const { nextTick } = require('process')
 
-app.use(bodyParser.urlencoded({ extended: false })) //built in parser
-app.use(express.static(path.join(__dirname, 'public'))) //static paths
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user
+        next()
+    })
+    .catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use(errorController.get404) 
+app.use(errorController.get404)
 
-sequelize.sync().then(result => {
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
+User.hasMany(Product)
+
+sequelize
+  .sync()
+  .then(result => {
+    User.findByPk(1)
     // console.log(result)
+    // app.listen(3000)
+  })
+  .then(user => {
+    if(!user) {
+        User.create({ name: 'Ethan', email: 'test@test.com'})
+    }
+    return user
+  })
+  .then(user => {
+    // console.log(user)
     app.listen(3000)
-})
-.catch(err => {console.log(err)})
-
+  })
+  .catch(err => {
+    console.log(err)
+  })
